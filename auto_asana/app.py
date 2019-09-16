bb092395d1e2911875329fb52df96f5dc00aa7ec
Steven Hsieh <stevenh6696@gmail.com>
2019-09-16 17:24:35 -0400

    Fetching fb events notified by webhook

--- auto_asana/app.py
+++ auto_asana/app.py
@@ -1,12 +1,9 @@
-import json
 import os
 import requests
 from flask import Flask, request, make_response
 
 app = Flask(__name__)
 secrets = []
-token = os.environ['TOKEN']
-me = os.environ['SELF']
 
 
 @app.route('/asana-webhook', methods=['POST'])
@@ -42,7 +39,8 @@ def asana_webhook():
             isTask = event.get('parent').get('resource_type') if event.get('parent') else None
             task = event.get('parent').get('gid') if event.get('parent') else None
 
-            if user == me and resourceSubtype == 'added_to_project' and isTask == 'task':
+            if user == os.environ['ASANA_SELF'] and resourceSubtype == 'added_to_project' \
+                    and isTask == 'task':
 
                 # Log new task
                 print('Asana_webhook: new task found:', task)
@@ -50,7 +48,7 @@ def asana_webhook():
                 # Set up post request
                 url = 'https://app.asana.com/api/1.0/tasks/' + task
                 data = {'assignee': 'me'}
-                headers = {'Authorization': 'Bearer ' + token}
+                headers = {'Authorization': 'Bearer ' + os.environ['ASANA_TOKEN']}
 
                 # Assign to self
                 result = requests.put(url, data=data, headers=headers)
@@ -68,6 +66,55 @@ def asana_webhook():
     else:
         print('Asana_webhook: neither secret nor signature was found')
         raise KeyError
+
+
+@app.route('/fb-webhook', methods=['GET', 'POST'])
+def fb_webhook():
+
+    # Complete creation of new webhook
+    if request.method == 'GET' and request.args.get('hub.mode') == 'subscribe':
+
+        # Log new webhook
+        challenge = request.args.get('hub.challenge')
+        print('Fb_webhook: webhook added with challenge', challenge)
+
+        # Return the challenge
+        return challenge, 200
+
+    # Process new events
+    elif request.method == 'POST' and request.json.get('entry'):
+
+        # Process each event
+        for event in request.json.get('entry'):
+
+            # Process each change
+            for change in event.get('changes'):
+
+                # Get event ID, action, and field
+                event = change.get('value', {}).get('event_id')
+                verb = change.get('value', {}).get('verb')
+                field = change.get('field')
+
+                # Confirm expected fields
+                if event and verb == 'accept' and field == 'events':
+
+                    # Log new event
+                    print('Fb_webhook: new event found:', event)
+
+                    # Set up get request
+                    url = 'https://graph.facebook.com/v3.3/' + event
+                    headers = {'Authorization': 'Bearer ' + os.environ['FB_USER']}
+
+                    # Query for more information about the event
+                    result = requests.get(url, headers=headers)
+
+                    # Process the query
+                    if result.ok:
+                        print('Fb_webhook: event logged:', result.json())
+                    else:
+                        print('Fb_webhook: event failed:', event)
+
+        return '', 200
 
 
 if __name__ == '__main__':
