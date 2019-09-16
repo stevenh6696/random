fd9dfd02836f03f89f71aa6d666ae1fef37a9403
Steven Hsieh <stevenzhsieh@gmail.com>
2019-09-16 02:59:18 -0400

    Filtering for new events correctly

--- auto_asana/app.py
+++ auto_asana/app.py
@@ -3,8 +3,9 @@ import os
 from flask import Flask, request, make_response
 
 app = Flask(__name__)
-secrets = os.environ['SECRETS']
+secrets = []
 token = os.environ['TOKEN']
+me = os.environ['SELF']
 
 
 @app.route('/asana-webhook', methods=['POST'])
@@ -16,16 +17,34 @@ def asana_webhook():
         # Log new webhook
         print('Asana_webhook: webhook added with secret', request.headers['X-Hook-Secret'])
 
+        # TODO: keep secrets
+
         # Return secret to complete handshake
         response = make_response('', 200)
         response.headers['X-Hook-Secret'] = request.headers['X-Hook-Secret']
         return response
 
     # Process the new events
-    elif 'X-Hook-Signature' in request.headers and request.headers['X-Hook-Signature'] in secrets:
+    elif 'X-Hook-Signature' in request.headers:
 
         # Log request
         print('Asana_webhook: new post received:', request.json)
+
+        # TODO: verify request
+
+        # Find new events
+        for event in request.json.get('events'):
+
+            # Check user, action, resource type, task, and parent type
+            user = event.get('user', {}).get('gid')
+            action = event.get('action')
+            resourceType = event.get('resource', {}).get('resource_type')
+            task = event.get('resource', {}).get('gid')
+            parentType = event.get('parent').get('resource_type') if event.get('parent') else None
+
+            if user == me and action == 'added' and resourceType == 'task' and parentType == 'project':
+                print(event)
+
         return '', 200
 
     # Error if neither header exists
